d4cd1627d410af722af27935df40ad7a6e66d55f
gongxuesong <gongxuesong@DESKTOP-PFQ0HGC>
2018-03-19 09:54:08 +0800

    简单记录相关源码图片以及类

---
+++ src/main/java/showcase/dw/glbg/mr/SourceCodeTest.java
@@ -0,0 +1,10 @@
+package showcase.dw.glbg.mr;
+
+public class SourceCodeTest {
+
+	public static void main(String[] args) {
+//		DataBlockScanner 一个在datanode上检查块数据异常的线程
+//		NameNode
+//		DataNode
+	}
+}
